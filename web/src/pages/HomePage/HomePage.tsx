0ec4f99fe4fdad5857b987e6bfc6acaab567ed46
Ian McPhail <ian@clerk.dev>
2022-02-23 18:57:45 -0500

    Add links and logos to HomePage

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,6 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import { SignedIn, SignedOut } from '@clerk/clerk-react'
 
 import './HomePage.css'
 
@@ -7,49 +8,89 @@ const HomePage = () => {
   return (
     <>
       <MetaTags title="Clerk + RedwoodJS Starter" description="Home page" />
-      <main>
+      <div className="home">
         <h1>Welcome to your new app</h1>
         <p>
           Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
         </p>
         <div className="cards">
-          <div className="card"></div>
+          <SignedIn>
+            <div className="card">
+              <Link to={routes.user()}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  stroke="#335bf1"
+                  strokeWidth={2}
+                >
+                  <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
+                  <path d="M3 9h18M9 21V9" />
+                </svg>
+                <div className="card-content">
+                  <h3>Explore features provided by Clerk</h3>
+                  <p>
+                    Interact with the user button, user profile, and more to
+                    preview what your users will see
+                  </p>
+                </div>
+                <svg
+                  className="icon-arrow"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  stroke="#335bf1"
+                  strokeWidth={2}
+                >
+                  <path d="M5 12h14M12 5l7 7-7 7" />
+                </svg>
+              </Link>
+            </div>
+          </SignedIn>
+          <SignedOut>
+            <div className="card">
+              <Link to={routes.signUp()}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  stroke="#335bf1"
+                  strokeWidth={2}
+                >
+                  <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2" />
+                  <circle cx="8.5" cy="7" r="4" />
+                  <path d="M20 8v6M23 11h-6" />
+                </svg>
+                <div className="card-content">
+                  <h3>Sign up for an account</h3>
+                  <p>
+                    Sign up and sign in to explore all the features provided by
+                    Clerk out-of-the-box
+                  </p>
+                </div>
+                <svg
+                  className="icon-arrow"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  stroke="#335bf1"
+                  strokeWidth={2}
+                >
+                  <path d="M5 12h14M12 5l7 7-7 7" />
+                </svg>
+              </Link>
+            </div>
+          </SignedOut>
           <div className="card">
-            <Link>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-                stroke="#335bf1"
-                strokeWidth={2}
-              >
-                <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2" />
-                <circle cx="8.5" cy="7" r="4" />
-                <path d="M20 8v6M23 11h-6" />
-              </svg>
-              <div className="card-content">
-                <h3>Sign up for an account</h3>
-                <p>
-                  Sign up and sign in to explore all the features provided by
-                  Clerk out-of-the-box
-                </p>
-              </div>
-              <svg
-                className="icon-arrow"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-                stroke="#335bf1"
-                strokeWidth={2}
-              >
-                <path d="M5 12h14M12 5l7 7-7 7" />
-              </svg>
-            </Link>
-          </div>
-          <div className="card">
-            <Link to="https://dashboard.clerk.dev">
+            <a
+              href="https://dashboard.clerk.dev?utm_source=clerk-redwood-starter"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -79,12 +120,12 @@ const HomePage = () => {
               >
                 <path d="M5 12h14M12 5l7 7-7 7" />
               </svg>
-            </Link>
+            </a>
           </div>
         </div>
         <div className="links">
           <a
-            href="https://docs.clerk.dev"
+            href="https://docs.clerk.dev?utm_source=clerk-redwood-starter"
             target="_blank"
             rel="noreferrer noopener"
           >
@@ -98,7 +139,29 @@ const HomePage = () => {
             Read RedwoodJS documentation
           </a>
         </div>
-      </main>
+        <footer className="footer">
+          <p>
+            Powered by{' '}
+            <a
+              href="https://clerk.dev?utm_source=clerk-redwood-starter"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Clerk"
+            >
+              <img alt="Clerk logo" src="/images/clerk.svg" />
+            </a>
+            +
+            <a
+              href="https://redwoodjs.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="RedwoodJS"
+            >
+              <img alt="RedwoodJS logo" src="/images/redwoodjs.svg" />
+            </a>
+          </p>
+        </footer>
+      </div>
     </>
   )
 }
