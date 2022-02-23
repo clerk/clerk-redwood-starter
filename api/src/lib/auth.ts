592ba70f1bd2bd4b8514369ca96dd937dd7fe160
Ian McPhail <ian@clerk.dev>
2022-02-23 17:59:25 -0500

    Add Clerk auth

--- api/src/lib/auth.ts
+++ api/src/lib/auth.ts
@@ -1,25 +1,101 @@
+import { parseJWT } from '@redwoodjs/api'
+import { AuthenticationError, ForbiddenError } from '@redwoodjs/graphql-server'
+import { logger } from 'src/lib/logger'
+
 /**
- * Once you are ready to add authentication to your application
- * you'll build out requireAuth() with real functionality. For
- * now we just return `true` so that the calls in services
- * have something to check against, simulating a logged
- * in user that is allowed to access that service.
+ * getCurrentUser returns the user information together with
+ * an optional collection of roles used by requireAuth() to check
+ * if the user is authenticated or has role-based access
  *
- * See https://redwoodjs.com/docs/authentication for more info.
+ * @param decoded - The decoded access token containing user info and JWT claims like `sub`. Note could be null.
+ * @param { token, SupportedAuthTypes type } - The access token itself as well as the auth provider type
+ * @param { APIGatewayEvent event, Context context } - An object which contains information from the invoker
+ * such as headers and cookies, and the context information about the invocation such as IP Address
+ *
+ * @see https://github.com/redwoodjs/redwood/tree/main/packages/auth for examples
+ */
+export const getCurrentUser = async (
+  decoded,
+  { _token, _type },
+  { _event, _context }
+) => {
+  if (!decoded) {
+    logger.warn('Missing decoded user')
+    return null
+  }
+
+  const { roles } = parseJWT({ decoded })
+
+  if (roles) {
+    return { ...decoded, roles }
+  }
+
+  return { ...decoded }
+}
+
+/**
+ * The user is authenticated if there is a currentUser in the context
+ *
+ * @returns {boolean} - If the currentUser is authenticated
  */
 export const isAuthenticated = () => {
+  return !!context.currentUser
+}
+
+/**
+ * When checking role membership, roles can be a single value, a list, or none.
+ * You can use Prisma enums too (if you're using them for roles), just import your enum type from `@prisma/client`
+ */
+
+/**
+ * Checks if the currentUser is authenticated (and assigned one of the given roles)
+ *
+ * @param roles: AllowedRoles - Checks if the currentUser is assigned one of these roles
+ *
+ * @returns {boolean} - Returns true if the currentUser is logged in and assigned one of the given roles,
+ * or when no roles are provided to check against. Otherwise returns false.
+ */
+export const hasRole = ({ roles }) => {
+  if (!isAuthenticated()) {
+    return false
+  }
+
+  if (roles) {
+    if (Array.isArray(roles)) {
+      return context.currentUser.roles?.some((r) => roles.includes(r))
+    }
+
+    if (typeof roles === 'string') {
+      return context.currentUser.roles?.includes(roles)
+    }
+
+    // roles not found
+    return false
+  }
+
   return true
 }
 
-export const hasRole = ({ roles }) => {
-  return roles !== undefined
-}
-
-// This is used by the redwood directive
-// in ./api/src/directives/requireAuth
-
-// Roles are passed in by the requireAuth directive if you have auth setup
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+/**
+ * Use requireAuth in your services to check that a user is logged in,
+ * whether or not they are assigned a role, and optionally raise an
+ * error if they're not.
+ *
+ * @param roles: AllowedRoles - When checking role membership, these roles grant access.
+ *
+ * @returns - If the currentUser is authenticated (and assigned one of the given roles)
+ *
+ * @throws {AuthenticationError} - If the currentUser is not authenticated
+ * @throws {ForbiddenError} If the currentUser is not allowed due to role permissions
+ *
+ * @see https://github.com/redwoodjs/redwood/tree/main/packages/auth for examples
+ */
 export const requireAuth = ({ roles }) => {
-  return isAuthenticated()
+  if (!isAuthenticated()) {
+    throw new AuthenticationError("You don't have permission to do that.")
+  }
+
+  if (!hasRole({ roles })) {
+    throw new ForbiddenError("You don't have access to do that.")
+  }
 }
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,5 @@
+import { AuthProvider } from '@redwoodjs/auth'
+import { ClerkProvider, withClerk } from '@clerk/clerk-react'
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
@@ -6,12 +8,46 @@ import Routes from 'src/Routes'
 
 import './index.css'
 
+// Wrap <ClerkAuthProvider> around the Redwood <AuthProvider>
+//
+// You can set user roles in a "roles" array on the public metadata in Clerk.
+//
+// Also, you need to add two env variables: CLERK_FRONTEND_API_URL for web and
+// CLERK_API_KEY for api, with the frontend api host and api key, respectively,
+// both from your Clerk.dev dashboard.
+//
+// Lastly, be sure to add the key "CLERK_FRONTEND_API_URL" in your app's redwood.toml
+// [web] config "includeEnvironmentVariables" setting.
+
+const ClerkAuthConsumer = withClerk(({ children, clerk }) => {
+  return React.cloneElement(children as React.ReactElement<any>, {
+    client: clerk,
+  })
+})
+
+const ClerkAuthProvider = ({ children }) => {
+  const frontendApi = process.env.CLERK_FRONTEND_API_URL
+  if (!frontendApi) {
+    throw new Error('Need to define env variable CLERK_FRONTEND_API_URL')
+  }
+
+  return (
+    <ClerkProvider frontendApi={frontendApi}>
+      <ClerkAuthConsumer>{children}</ClerkAuthConsumer>
+    </ClerkProvider>
+  )
+}
+
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <RedwoodApolloProvider>
-        <Routes />
-      </RedwoodApolloProvider>
+      <ClerkAuthProvider>
+        <AuthProvider type="clerk">
+          <RedwoodApolloProvider>
+            <Routes />
+          </RedwoodApolloProvider>
+        </AuthProvider>
+      </ClerkAuthProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
 )
